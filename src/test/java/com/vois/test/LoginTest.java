b35a389e6e3936a1c7efa13bb1dcdd8c5dbae887
kod21 <satvinder272@gmail.com>
2025-05-08 04:57:51 -0700

    Updated code with excel

--- src/test/java/com/vois/test/LoginTest.java
+++ src/test/java/com/vois/test/LoginTest.java
@@ -1,6 +1,5 @@
 package com.vois.test;
 
-import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -12,31 +11,27 @@ import com.vois.utils.DataUtils;
 public class LoginTest extends AutomationWrapper {
 	
 	
-	@Test
-	public void validLoginTest()
-	{
-		LoginPage login= new LoginPage(driver);
-		login.enterUsername("Admin");
-		login.enterPassword("admin123");
-		login.clickOnLogin();
-		//Assert 
-		DashboardPage dashboard=new DashboardPage(driver);
-		Assert.assertEquals(dashboard.getQuickLaunchText(),("Quick Launch"));
-	}
-	
-	
-	
-	@Test(dataProviderClass=DataUtils.class,dataProvider="invalidLoginData")
-	public void invalidLoginTest()
-	{
-		LoginPage login= new LoginPage(driver);
-		login.enterUsername("John");
-		login.enterPassword("admin123");
-		login.clickOnLogin();
-		
-		String actualError=driver.findElement(By.xpath("//p[contains(normalize-space(),'Invalid credentials')]")).getText();
-		Assert.assertEquals(actualError, "Invalid credentials");
-	}
+	@Test(dataProviderClass = DataUtils.class,dataProvider = "commonDataProvider")
+	 public void validLoginTest(String username,String password,String expectedValue) {
+	  LoginPage login = new LoginPage(driver);
+	  login.enterUsername(username);
+	  login.enterPassword(password);
+	  login.clickOnLogin();
+
+	  DashboardPage dashboard = new DashboardPage(driver);
+	  Assert.assertEquals(dashboard.getQuickLaunchText(), expectedValue);
+	 }
+
+	 @Test(dataProviderClass = DataUtils.class,dataProvider = "commonDataProvider")
+	 public void invalidLoginTest(String username, String password, String expectedError) {
+	  LoginPage login = new LoginPage(driver);
+	  login.enterUsername(username);
+	  login.enterPassword(password);
+	  login.clickOnLogin();
+
+	  String actualError = login.getInvalidErrorMessage();
+	  Assert.assertEquals(actualError, expectedError);
+	 }
 	
 
 }
