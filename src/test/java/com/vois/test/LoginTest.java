7ad3ee2a89b98cca7784c9dff6e8663e0df17cad
kod21 <satvinder272@gmail.com>
2025-05-07 23:20:44 -0700

    Added LoginTest

--- src/test/java/com/vois/test/LoginTest.java
+++ src/test/java/com/vois/test/LoginTest.java
@@ -1,6 +1,7 @@
 package com.vois.test;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.voic.base.AutomationWrapper;
@@ -13,7 +14,20 @@ public class LoginTest extends AutomationWrapper {
 	{
 		driver.findElement(By.name("username")).sendKeys("Admin");
 		driver.findElement(By.name("password")).sendKeys("admin123");
-		driver.findElement(By.xpath("//button[normalise-space()='Login']")).click();
+		driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
+		//Assert 
+		String actualText=driver.findElement(By.xpath("//p[contains(normalize-space,'Quick')]")).getText();
+		Assert.assertTrue(actualText.contains("Quick Launch"));
+	}
+	
+	public void invalidLoginTest()
+	{
+		driver.findElement(By.name("username")).sendKeys("john");
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
+		
+		String actualError=driver.findElement(By.xpath("//p[contains(normalize-space(),'Invalid credentials')]")).getText();
+		Assert.assertEquals(actualError, "Invalid credentials");
 	}
 	
 
--- src/test/java/com/vois/test/LoginUITest.java
+++ src/test/java/com/vois/test/LoginUITest.java
@@ -1,33 +1,13 @@
 package com.vois.test;
 
-import java.time.Duration;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class LoginUITest {
-	WebDriver driver;
+import com.voic.base.AutomationWrapper;
+
+public class LoginUITest extends AutomationWrapper{
 	
-	@BeforeMethod
-	public void setup()
-	{
-		 driver = new ChromeDriver();
-		  driver.manage().window().maximize();
-		  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		  
-		  driver.get("https://opensource-demo.orangehrmlive.com/");	
-	}
-	
-	@AfterMethod
-	public void tearDown()
-	{
-		driver.quit();
-	}
 	
 	@Test
 	public void titleTest()
